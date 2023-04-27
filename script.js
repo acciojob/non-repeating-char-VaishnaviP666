b4941d0c42528366ab17a1b23910a06ce9cd5d27
VaishnaviP666 <122201508+VaishnaviP666@users.noreply.github.com>
2023-04-27 10:51:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+let index = -1;
+let fnc = ' ';
+for (let i of string) {
+    if (string.split(i).length - 1 === 1) {
+        fnc = i;
+        break;
+    } else {
+        index += 1;
+    }
+	return fnc;
+}
